aec994450a6e323bfea013c311932665bd7ff475
Abdelrahman Elnagdy <Abdelrahman.elnagdy.eng@gmail.com>
2021-02-04 18:26:39 +0200

    adding Login with google
    adding to the XML activity and the java activity of login and adding to the gradle

--- app/src/main/java/com/example/tripreminder/ui/activities/LoginActivity.java
+++ app/src/main/java/com/example/tripreminder/ui/activities/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.tripreminder.ui.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,12 @@ import android.widget.Toast;
 import com.example.tripreminder.R;
 import com.example.tripreminder.data.local.SharedPref;
 import com.example.tripreminder.ui.activities.MainActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -28,12 +36,17 @@ public class LoginActivity extends AppCompatActivity {
     TextView register;
     ProgressDialog progressDialog;
     FirebaseAuth fAuth;
-
+    GoogleSignInClient mGoogleSignInClient;
+   int RC_SIGN_IN = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
        initialize();
 //        if (fAuth.getCurrentUser()!= null){
@@ -79,7 +92,23 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        SignInButton signInButton = findViewById(R.id.google_sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
     public void initialize(){
         signIn = findViewById(R.id.btn_log_in);
         editEmail = findViewById(R.id.email_edit);
@@ -90,5 +119,34 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setMessage("Signing In please wait...");
         SharedPref.createPrefObject(LoginActivity.this);
 
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    public void updateUI(GoogleSignInAccount account){
+        if (account != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }  else {
+            Toast.makeText(this, "Please login with a valid Google account", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            updateUI(null);
+        }
     }
 }
