0ae32311fc6f7f7ef6726055766007cf0abf881e
AlshimaaMuhammad <alshimaamuhammed@gmail.com>
2021-02-10 16:13:34 +0200

    splash

--- app/src/main/java/com/example/tripreminder/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/tripreminder/ui/activities/SplashActivity.java
@@ -1,30 +1,44 @@
 package com.example.tripreminder.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 import com.example.tripreminder.R;
 import com.example.tripreminder.data.local.SharedPref;
 
-public class SplashActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
 
+public class SplashActivity extends AppCompatActivity {
+    private Animation textAnimation, layoutAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         init();
+
     }
 
     private void init(){
+        CircleImageView imageView =findViewById(R.id.logo);
+        textAnimation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.bottom_to_top);
+        imageView.setVisibility(View.VISIBLE);
+        imageView.setAnimation(textAnimation);
         // Wait for 3 seconds then open home.
         (new Handler()).postDelayed(() -> {
             // Open home.
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setAnimation(textAnimation);
             SharedPref.createPrefObject(SplashActivity.this);
             boolean isLogin =SharedPref.checkLogin();
-
+         //
             if(isLogin) {
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
@@ -36,4 +50,5 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 3000);
     }
+
 }
